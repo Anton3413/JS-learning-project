bdae40d43333101809bb548550fb3c004e0069c6
Anton3413 <bondar8524567913@gmail.com>
2025-04-04 17:58:46 +0200

    lesson 11

---
+++ src/main/resources/static/js/lesson11-functions.js
@@ -0,0 +1,36 @@
+function sayHi() {
+    console.log( "Привет" );
+}
+
+let sayHi2 = function() {
+    console.log( "Привет" );
+};
+
+
+
+function sum(val1, val2){
+    return val1+val2;
+}
+
+
+
+function sayHi(){
+    console.log( "Hi!");
+}
+
+let c = sayHi;
+
+
+c();
+
+
+function ask(question, yesFunc, noFunc){
+    if(confirm(question)){
+        yesFunc();
+    }else noFunc();
+}
+
+ask("Yes or no?",
+    function(){console.log("Confirmed")},
+    function (){console.log("Canseled")}
+);
